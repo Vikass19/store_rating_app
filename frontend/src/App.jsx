c8040b6313b6deba77b3466159690fa923c1e7fe
Vikas Bansode <131449554+Vikass19@users.noreply.github.com>
2025-09-18 22:56:42 +0530

    frontend building

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <h1 className='text-2xl text-black font-bold'>frontend building for store app </h1>
+    </div>
+  )
+}
+
+export default App
